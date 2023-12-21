99c432b5c242227e6495abd0773d6cc88e08cf55
adak99 <soumyadipadak99@gmail.com>
2023-12-21 21:39:25 +0530

    12/21/23 recursion

--- recursion_sum.java
+++ recursion_sum.java
@@ -1,15 +1,22 @@
-public class recursion_sum {
+public class recursion_sum { // method overloading
 
-    int add(int a, int b) {
-        int c = a + b;
-        return c;
+    int add(int num1, int num2) {
+
+        int c = num1 + num2;
+
+        if (c != 0 || c == 0)
+            return c;
+
+        else
+            return add(num1, num2);
     }
 
     public static void main(String[] args) {
 
         recursion_sum r = new recursion_sum();
 
-        int result = r.add(1, 2);
+        int result = r.add(0, 0);
+
         System.out.println("Answer:- " + result);
     }
 }
