154f866b759bd4f745c46777f81f90363ac2b64d
Soumya ! <soumyadipadak99@gmail.com>
2024-07-01 20:35:44 +0530

    update

--- print.java
+++ print.java
@@ -1,15 +1,20 @@
-public class print { // recursion
-    public static void printNumebr(int n) {
-        if (n == 0) {
-            return;
-        }
+public class print { // array
+    public static void printNumebr(int arr[], int n, int temp) {
 
-        System.out.print(n + " ");
-        printNumebr(n - 1);
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[n] = temp;
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        printNumebr(n);
+        int arr[] = { 1, 2, 3 };
+
+        printNumebr(arr, arr.length - 1, arr[0]);
+
+        for (int resutl : arr) {
+            System.out.println(resutl);
+        }
+
     }
 }
