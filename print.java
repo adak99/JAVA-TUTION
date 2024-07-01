d8d6699d405517d4078fbd67539040761e057065
Soumya ! <soumyadipadak99@gmail.com>
2024-07-01 20:22:26 +0530

    update

--- print.java
+++ print.java
@@ -1,5 +1,11 @@
 public class print {
     public static void main(String[] args) {
-
+        // print factorial of a number
+        int n = 5;
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = i * fact;
+        }
+        System.out.println(fact);
     }
 }
