0008961b2b510f605bb7c190bc0ac3f7a3c40e90
Soumya ! <soumyadipadak99@gmail.com>
2024-07-03 16:19:23 +0530

    update

--- practiceQuestion.java
+++ practiceQuestion.java
@@ -3,6 +3,7 @@ public class practiceQuestion {
         int n = 5;
 
         for (int i = 1; i <= n; i++) {
+            // lower part
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
@@ -15,6 +16,9 @@ public class practiceQuestion {
                 System.out.print("*");
             }
             System.out.println();
+
+            // lower part
+
         }
     }
 }
