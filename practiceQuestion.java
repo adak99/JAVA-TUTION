0ad7ea1c2641220077b41bba09202f29c064a35e
Soumya ! <soumyadipadak99@gmail.com>
2024-07-04 10:21:33 +0530

    update

--- practiceQuestion.java
+++ practiceQuestion.java
@@ -2,8 +2,8 @@ public class practiceQuestion {
     public static void main(String[] args) {
         int n = 5;
 
+        // upper part
         for (int i = 1; i <= n; i++) {
-            // lower part
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
@@ -16,9 +16,23 @@ public class practiceQuestion {
                 System.out.print("*");
             }
             System.out.println();
+        }
 
-            // lower part
-
+        // lower part
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // space
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            // print star
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
--- print.py
+++ print.py
@@ -1,14 +1,9 @@
 n = 5
-# m = 5
 
-# upper part
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        print("*", end="")
-    # print space
-    space = int(2 * (n - i))
-    for k in range(0, space):
+# Upper part
+for i in range(n + 1):
+    for j in range(0, n - i):
         print(" ", end="")
-    for l in range(1, i + 1):
-        print("*", end="")
-    print()
+    for k in range(1, i + 1):
+        print(" *", end="")
+    print("")
