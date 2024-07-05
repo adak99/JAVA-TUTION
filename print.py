c7fa7278b5420b7a68357497354b8cbdb84595d7
Soumya ! <soumyadipadak99@gmail.com>
2024-07-05 10:18:39 +0530

    upate

--- print.py
+++ print.py
@@ -1,11 +1,9 @@
 n = 5
 
 # Upper part
-for i in range(n + 1):
+for i in range(1, n + 1):
     for j in range(0, n - i):
         print(" ", end="")
     for k in range(1, i + 1):
         print(" *", end="")
-    print("")
-    
-for l in range(n,-1,1)
+    print()
