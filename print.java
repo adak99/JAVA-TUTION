a2ca20800d189392dddc144826af86833518d6d5
Soumya ! <soumyadipadak99@gmail.com>
2024-07-05 10:16:30 +0530

    upadate

--- print.java
+++ print.java
@@ -1,17 +1,18 @@
 public class print {
     public static void main(String[] args) {
         int n = 5;
-        // int m = 5;
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= i; j++) {
+        // upper part
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print("*");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
             }
             System.out.println();
         }
 
+        // lower part
     }
 }
--- print.py
+++ print.py
@@ -7,3 +7,5 @@ for i in range(n + 1):
     for k in range(1, i + 1):
         print(" *", end="")
     print("")
+    
+for l in range(n,-1,1)
