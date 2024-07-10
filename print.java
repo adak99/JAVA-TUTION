7db1566949faf9569c4be7b3704172dcc8eb4977
Soumya ! <soumyadipadak99@gmail.com>
2024-07-10 14:14:29 +0530

    update

--- print.java
+++ print.java
@@ -1,16 +1,19 @@
 public class print {
-    public static void calculation(int i, int n, int sum) {
-        if (i == n) {
-            sum += i;
-            System.out.println(sum);
+    public static void calculation(int a, int b, int n) {
+        if (n == 0) {
             return;
         }
 
-        sum += i;
-        calculation(i + 1, n, sum);
+        int c = a + b;
+        System.out.print(" " + c);
+        calculation(b, c, n - 1);
     }
 
     public static void main(String[] args) {
-        calculation(1, 5, 0);
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " " + b);
+        int n = 5;
+        calculation(a, b, n - 2);
     }
 }
