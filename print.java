68b9c83a813418f1242df85805cb6829e87a065e
Soumya ! <soumyadipadak99@gmail.com>
2024-07-10 11:59:15 +0530

    update

--- print.java
+++ print.java
@@ -1,25 +1,10 @@
 public class print {
     public static void main(String[] args) {
-        int n = 5;
+        int arr[][] = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
 
-        // upper part
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(" *");
-            }
-            System.out.println();
-        }
-
-        // lower part
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" *");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j]);
             }
             System.out.println();
         }
--- print.py
+++ print.py
@@ -1,22 +1,7 @@
-# a = list(map(int, input("Enter the value of a[]: ").split()))
-# b = list(map(int, input("Enter the value of b[]: ").split()))
-a = [1, 2, 3]
-b = [1, 2, 3]
-listEquals = True
+def sum(a, b):
+    sum = a + b
+    return sum
 
-if len(a) != len(b):
-    listEquals = False
-else:
-    for i in range(len(a)):
-        if a[i] != b[i]:
-            listEquals = False
-            break
 
-if listEquals:
-    print("{}".format(a))
-    print("{}".format(b))
-    print("EQUAL")
-else:
-    print("{}".format(a))
-    print("{}".format(b))
-    print("NOT EQUAL")
+total = sum(2, 3)
+print(total)
