38958094c5862734de49fad33f15c074548a24d4
Soumya ! <soumyadipadak99@gmail.com>
2024-07-19 08:34:45 +0530

    update

--- print.java
+++ print.java
@@ -1,32 +1,36 @@
-import java.util.HashSet;
-import java.util.Set;
-
 public class print {
-    public static int deleteDuplicate(int arr[], int arrSize) {
-        if (arrSize <= 1)
-            return arrSize;
 
-        Set<Integer> unicqElement = new HashSet<>();
+    public static void printOriginalArray(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
-        for (int i = 0; i < arrSize; i++)
-            unicqElement.add(arr[i]);
-
-        int index = 0;
-        for (int element : unicqElement)
-            arr[index++] = element;
-
-        return unicqElement.size();
-
+    public static void modifyMatrix(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i == 0 || j == 0 || i == arr.length - 1 || j == arr.length - 1)
+                    System.out.print(arr[i][j] + " ");
+                else
+                    System.out.print(" " + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 4, 2, 2, 3, 4 };
-        int arrSize = arr.length;
+        int arr[][] = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
 
-        arrSize = deleteDuplicate(arr, arrSize);
-
-        for (int i = 0; i < arrSize; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        System.out.println("Print original array: ");
+        printOriginalArray(arr);
+        System.out.println("Print modify array: ");
+        modifyMatrix(arr);
     }
 }
